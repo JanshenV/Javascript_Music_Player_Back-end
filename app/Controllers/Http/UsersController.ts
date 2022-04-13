d137ba6e674e13c5842b3cb4d814d6d8c12ddac5
Janshen Vernon <janshen.vbb@gmail.com>
2022-04-13 15:33:15 -0300

    Added function to get all users from database. Imported Model user to the Users controller

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,5 +1,13 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import User from 'App/Models/User';
 export default class UsersController {
+    public async index({ response }: HttpContextContract) {
+        try {
+            const allUsers = await User.all();
 
-}
+            return { allUsers }
+        } catch (error) {
+            return response.badRequest(error);
+        };
+    };
+};
