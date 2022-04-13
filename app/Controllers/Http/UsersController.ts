4d35ee478186da04180f00179879d43d81e798d8
Janshen Vernon <janshen.vbb@gmail.com>
2022-04-13 16:05:33 -0300

    added user edit for existing users and a new validation for updating those users

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -5,7 +5,7 @@ import User from 'App/Models/User';
 
 //Validators
 import CreateUserValidator from 'App/Validators/CreateUserValidator';
-
+import UpdateUserValidator from 'App/Validators/UpdateUserValidator';
 
 export default class UsersController {
     public async index({ response }: HttpContextContract) {
@@ -59,4 +59,55 @@ export default class UsersController {
             return response.badRequest(error);
         };
     };
+
+    public async update({ params, request, response }: HttpContextContract) {
+        const { username, email, password } = request.body();
+        const { id } = params;
+
+        try {
+            if (!id) return response.badRequest({
+                message: 'User not found.'
+            });
+
+            const user = await User.findBy('id', id);
+            if (!user) return response.badRequest({
+                message: 'User not found.'
+            });
+
+            if (username) {
+                await request.validate(UpdateUserValidator);
+                user.username = username;
+            };
+
+            if (email && email !== user.email) {
+                await request.validate(UpdateUserValidator);
+                const existingEmail = await User.findBy('email', email);
+                if (existingEmail) return response.badRequest({
+                    message: 'Email already in use.'
+                });
+                user.email = email.toLowerCase();
+            };
+
+            if (password) {
+                await request.validate(UpdateUserValidator);
+                user.password = password;
+            };
+
+            await user.save();
+
+            const userUpdated = {
+                username: user.username,
+                email: user.email
+            };
+
+            return response.status(200).json({
+                message: 'User updated successfully',
+                user: userUpdated
+            });
+
+        } catch (error) {
+            return response.badRequest(error);
+        };
+    };
+}
 };
---
+++ app/Validators/UpdateUserValidator.ts
@@ -0,0 +1,59 @@
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class UpdateUserValidator {
+  constructor(protected ctx: HttpContextContract) { }
+
+  /*
+   * Define schema to validate the "shape", "type", "formatting" and "integrity" of data.
+   *
+   * For example:
+   * 1. The username must be of data type string. But then also, it should
+   *    not contain special characters or numbers.
+   *    ```
+   *     schema.string({}, [ rules.alpha() ])
+   *    ```
+   *
+   * 2. The email must be of data type string, formatted as a valid
+   *    email. But also, not used by any other user.
+   *    ```
+   *     schema.string({}, [
+   *       rules.email(),
+   *       rules.unique({ table: 'users', column: 'email' }),
+   *     ])
+   *    ```
+   */
+  public schema = schema.create({
+    username: schema.string({ trim: true }, [
+      rules.minLength(3),
+    ]),
+
+    email: schema.string({ trim: true }, [
+      rules.normalizeEmail({}),
+      rules.email({}),
+    ]),
+
+    password: schema.string({}, [
+      rules.minLength(9)
+    ])
+  })
+
+  /**
+   * Custom messages for validation failures. You can make use of dot notation `(.)`
+   * for targeting nested fields and array expressions `(*)` for targeting all
+   * children of an array. For example:
+   *
+   * {
+   *   'profile.username.required': 'Username is required',
+   *   'scores.*.number': 'Define scores as valid numbers'
+   * }
+   *
+   */
+  public messages = {
+    'username.minLength': 'Username length must be higher or equals to three.',
+
+    'email.email': 'Email must be a proper email.',
+
+    'password.minLength': 'Password length must be higher or equals to nine.',
+  }
+}
